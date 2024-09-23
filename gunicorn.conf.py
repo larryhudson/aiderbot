bd5e248de9ebe50b8e1f6a72624f26140e0e01ba
Larry Hudson <10627494+larryhudson@users.noreply.github.com>
2024-09-23 22:48:05 +0200

    update gunicorn timeout to 5 mins

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,13 +1,13 @@
 # Gunicorn configuration file
 
-# Increase the worker timeout to 120 seconds (2 minutes)
-timeout = 120
+# Increase the worker timeout to 300 seconds (5 minutes)
+timeout = 300
 
 # Number of worker processes
 workers = 4
 
 # Bind to localhost on port 5000
-bind = "127.0.0.1:5000"
+bind = "127.0.0.1:5001"
 
 # Set the worker class to 'sync'
 worker_class = 'sync'
